798611b6d44f6fb32d7ed5e0c6b90f3485d34930
Jae-Kyu Choi <choi3961@naver.com>
2013-09-13 07:44:20 +0900

    Added main index file(raw)

---
+++ index.php
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE>
+<html>
+<head>
+<title>
+Welcome to p1
+</title>
+</head>
+<body>
+<header>Welcome to p1</header>
+<h1>My name</h1>
+<p></p>
+<h1>My information</h1>
+<p></p>
+<h1>My editor</h1>
+<p></p>
+</body>
+</html>
