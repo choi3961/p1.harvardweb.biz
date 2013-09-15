a3f415c2833d93514f86cfc709b2801c97a69826
Jae-Kyu Choi <choi3961@naver.com>
2013-09-15 14:12:03 +0900

    validated(index.php, main.css)

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE>
+﻿<!DOCTYPE html>
 <html>
 	<head>
 		<title>
@@ -7,20 +7,18 @@
 		<link rel="stylesheet" type="text/css" href="main.css">
 	</head>
 	<body>
-		<div align="center" >
-			<div class="container">
-				<header>
-					<h1>Welcome to p1</h1>
-				</header>
-				<h2>My name</h2>
-				<p>Hello, my name is Jae-Kyu Choi</p>
-				<h2>My information</h2>
-				<p>
-					I live in South Korea. I am running an institute.
-				</p>
-				<h2>My editor</h2>
-				<p>I am currently using Sublime Text2</p>
-			</div>
+		<div class="container">
+			<header>
+				<h1>Welcome to p1</h1>
+			</header>
+			<h2>My name</h2>
+			<p>Hello, my name is Jae-Kyu Choi</p>
+			<h2>My information</h2>
+			<p>
+				I live in South Korea. I am running an institute.
+			</p>
+			<h2>My editor</h2>
+			<p>I am currently using Sublime Text2</p>
 		</div>
 	</body>
 </html>
