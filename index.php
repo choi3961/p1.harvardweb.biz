3ad277d302d7a526d0df3a561cb12748516b669c
Jae-Kyu Choi <choi3961@naver.com>
2013-09-15 13:06:15 +0900

    revised, not yet validated (index.php,main.css)

--- index.php
+++ index.php
@@ -2,23 +2,25 @@
 <html>
 	<head>
 		<title>
-			Projec1 for the Dynamic Web
+			Projec1 for the Dynamic Web 
 		</title>
 		<link rel="stylesheet" type="text/css" href="main.css">
 	</head>
 	<body>
-		<div>
-			<header>
-				<h1>Welcome to p1</h1>
-			</header>
-			<h2>My name</h2>
-			<p>Hello, my name is Jae-Kyu Choi</p>
-			<h2>My information</h2>
-			<p>
-				I live in South Korea. I am running an institute.
-			</p>
-			<h2>My editor</h2>
-			<p>I am currently using Sublime Text2</p>
+		<div align="center" >
+			<div class="container">
+				<header>
+					<h1>Welcome to p1</h1>
+				</header>
+				<h2>My name</h2>
+				<p>Hello, my name is Jae-Kyu Choi</p>
+				<h2>My information</h2>
+				<p>
+					I live in South Korea. I am running an institute.
+				</p>
+				<h2>My editor</h2>
+				<p>I am currently using Sublime Text2</p>
+			</div>
 		</div>
 	</body>
 </html>
